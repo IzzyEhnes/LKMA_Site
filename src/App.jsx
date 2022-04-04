7bd09f853c78a3746cbf425753a172cc0b86fb47
Izzy Ehnes <izzyehnes@gmail.com>
2022-04-03 20:08:21 -0700

    Rename Services to Programs

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { Navigation } from "./components/navigation";
 import { Header } from "./components/header";
 import { Features } from "./components/features";
 import { About } from "./components/about";
-import { Services } from "./components/services";
+import { Programs } from "./components/programs";
 import { Gallery } from "./components/gallery";
 import { Testimonials } from "./components/testimonials";
 import { Team } from "./components/Team";
@@ -29,7 +29,7 @@ const App = () => {
       <Header data={landingPageData.Header} />
       <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
-      <Services data={landingPageData.Services} />
+      <Programs data={landingPageData.Programs} />
       <Gallery data={landingPageData.Gallery}/>
       <Testimonials data={landingPageData.Testimonials} />
       <Team data={landingPageData.Team} />
