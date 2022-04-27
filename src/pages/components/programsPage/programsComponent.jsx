b7cec1eea936457ee1efddff467665953ebdb075
Izzy Ehnes <30813436+IzzyEhnes@users.noreply.github.com>
2022-04-26 19:20:55 -0700

    LKMAW-87
    * Update programsComponent to display program information
    
    * Add images for programs
    
    * Move images for programs on homepage to img/homePage/programs
    
    * Add content to programsData.json
    
    * Add styling for program details section
    
    * Fix backend connectivity for Programs page
    
    * Fix program detail content responsiveness
    
    * Update horizontal rules to match theme
    
    * Update filepath for Programs images

--- src/pages/components/programsPage/programsComponent.jsx
+++ src/pages/components/programsPage/programsComponent.jsx
@@ -1,14 +1,35 @@
 export const ProgramsComponent = (props) => {
     return (
-      <div className='text-center'>
-        <div className='container'>
-          <div className='section-title'>
-            <h2>Programs</h2>
-            <p>
-              Placeholder Programs Page
-            </p>
-          </div>
+      <div id='program-details' className='text-center'>
+      <div className='container'>
+        <div className='row'>
+            {props.data
+              ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4 program-details-item'>
+                    <div className='programs-title'>
+                      <h2>{d.name}</h2>
+                      <img src={d.img} alt='...'/>
+                      <p>{d.paragraph}</p>
+                    </div>
+                    <div>
+                      {d.subPrograms && d.subPrograms.length && d.subPrograms.map((subprogram, k) => {
+                        return (
+                          <div key={k} className='subprogram-details-item'>
+                            <hr></hr>
+                            <h2>{subprogram.subProgramName}</h2>
+                            <h4>{subprogram.subProgramAgeRange}</h4>
+                            <p>{subprogram.subProgramParagraph1}</p>
+                            <img src={subprogram.img} alt='...'/>
+                            <p>{subprogram.subProgramParagraph2}</p>
+                          </div>
+                        );
+                      })}
+                    </div> 
+                </div>
+              ))
+              : 'Loading...'}
         </div>
       </div>
-    )
+    </div>
+  )
 }
