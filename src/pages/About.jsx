2f37fdb65c21796f8e8aa63a1f244a6f79b69111
giobarrientos <giovannibarrien@csus.edu>
2022-04-30 15:47:43 -0700

    integrated youtube player on about page. need to find a way to center it correctly

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { Instructors } from "./components/aboutPage/instructors";
 import JsonData from "./data/aboutData.json";
 import SmoothScroll from "smooth-scroll";
+import YoutubePlayer from "./components/aboutPage/YoutubePlayer"
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
@@ -17,6 +18,7 @@ const About = () => {
   return (
     <div>
       <Instructors data={aboutPageData.Instructors} />
+      <YoutubePlayer videoID='d0akqRlyjo8' />
     </div>
   );
 };
