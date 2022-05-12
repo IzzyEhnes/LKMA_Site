5e63f0f5758fefe79612c184bfb4b19ab79f2da2
giobarrientos <giovannibarrien@csus.edu>
2022-05-12 03:34:55 -0700

    commenting out import on YoutubePlayer

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import { Instructors } from "./components/aboutPage/instructors";
 import JsonData from "./data/aboutData.json";
 import SmoothScroll from "smooth-scroll";
-import YoutubePlayer from "./components/aboutPage/YoutubePlayer"
+{/*import YoutubePlayer from "./components/aboutPage/YoutubePlayer"*/}
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
