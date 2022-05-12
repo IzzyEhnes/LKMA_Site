076d3d81e4288e15a1e572f8c44bac7637718aa1
giobarrientos <giovannibarrien@csus.edu>
2022-05-12 03:39:22 -0700

    removing import

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,6 @@ import { useState, useEffect } from "react";
 import { Instructors } from "./components/aboutPage/instructors";
 import JsonData from "./data/aboutData.json";
 import SmoothScroll from "smooth-scroll";
-{/*import YoutubePlayer from "./components/aboutPage/YoutubePlayer"*/}
 
 export const scroll = new SmoothScroll('a[href*="#"]', {
   speed: 1000,
