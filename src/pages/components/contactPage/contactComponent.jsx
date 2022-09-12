e1cd54dd97392b65e4c87c40705ef91f12a1f1cb
izzy <izzyehnes@gmail.com>
2022-09-12 12:53:36 -0700

    LKMAW-33

---
+++ src/pages/components/contactPage/contactComponent.jsx
@@ -0,0 +1,19 @@
+export const ContactComponent = (props) => {
+  return (
+    <div id='contact' className='text-center'>
+      <div className='container'>
+        <div className='contact-title'>
+          <h2>Contact Us</h2>
+        </div>
+        <div className='row'>
+          <h3>
+            Have any questions or comments?
+            <br></br>
+            <br></br>
+            You can get in touch with us by filling out this <a href="https://docs.google.com/forms/d/e/1FAIpQLSfYlRNzFTCSV5Tq9460DD0JgHMLXI4s1GEGs_oQnghPRGUB2A/viewform" target="_blank">Information Request Form</a>.
+          </h3>
+        </div>
+      </div>
+    </div>
+  )
+}
