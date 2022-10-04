1b4b7beeb8844f81f940a6a58fac8a470653bcf4
jjrv360 <jjrv360@gmail.com>
2022-10-04 12:52:09 -0700

    LKMAW-57, LKMAW-58, LKMAW-59

--- src/pages/components/loginPage/loginComponent.jsx
+++ src/pages/components/loginPage/loginComponent.jsx
@@ -1,6 +1,43 @@
 import { NavLink } from "react-router-dom";
+import {useRef} from 'react';
+
+const validateEmail = (email) => {
+  return String(email)
+    .toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
 
 export const LoginComponent = (props) => {
+
+  const inputEmail=useRef(null);
+  const inputPassword=useRef(null);
+
+  function validate() {
+    if(inputEmail.current.value === ""){
+      console.log("No email provided")
+      document.getElementById("emailError").innerHTML = "Please provide an email"
+    }else if(validateEmail(inputEmail.current.value)){
+      console.log("Email valid")
+      document.getElementById("emailError").innerHTML = ""
+    }else{
+      console.log("Email invalid")
+      document.getElementById("emailError").innerHTML = "Please enter a valid email"
+    }
+
+    if(inputPassword.current.value === ""){
+      console.log("No password provided")
+      document.getElementById("passwordError").innerHTML = "Please provide a password"
+    }else if(inputPassword.current.value.length > 7){
+      console.log("Valid Password")
+      document.getElementById("passwordError").innerHTML = ""
+    }else{
+      console.log("Password must be 8 characters or longer in length")
+      document.getElementById("passwordError").innerHTML = "Password must be 8 characters or greater in length"
+    }
+  }
+
     return (
       <div id='login' className='text-center'>
         <div className='container'>
@@ -9,10 +46,12 @@ export const LoginComponent = (props) => {
                 <div class="form-container sign-in-container">
                     <form action="#">
                         <h1>Sign in</h1>
-                        <input type="email" placeholder="Email" />
-                        <input type="password" placeholder="Password" />
+                        <div id="emailError"></div>
+                        <div id="passwordError"></div>
+                        <input ref={inputEmail} id="email" type="email" placeholder="Enter your email" required/>
+                        <input ref={inputPassword} id="password" type="password" placeholder="Password" minlength="8" required/>
                         <a href="#">Forgot your password?</a>
-                        <button>Sign In</button>
+                        <button type="button" onClick={validate} >Sign In</button>
                     </form>
                 </div>
                 <div class="overlay-container">
