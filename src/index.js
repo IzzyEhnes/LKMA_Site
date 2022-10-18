6f065c83632b2fb18651875e756719def3915340
jjrv360 <jjrv360@gmail.com>
2022-10-18 13:23:27 -0700

    LKMAW-150

--- src/index.js
+++ src/index.js
@@ -14,6 +14,7 @@ import Profile from './pages/Profile';
 import SignUp from './pages/SignUp';
 import Login from './pages/Login';
 import ResetPassword from './pages/ResetPassword';
+import ChangeEmail from './pages/ChangeEmail';
 import Temp from './pages/Temp';
 import Forgot from './pages/Forgot';
 import Admin from './pages/Admin';
@@ -35,6 +36,7 @@ ReactDOM.render(
         <Route path="/signup" element={<SignUp />} />
         <Route path="/login" element={<Login />} />
         <Route path="/resetpassword" element={<ResetPassword />} />
+        <Route path="/changeemail" element={<ChangeEmail />} />
         <Route path="/temp" element={<Temp />} />
         <Route path="/forgot" element={<Forgot />} />
         <Route path="/admin" element={<Admin />} />
