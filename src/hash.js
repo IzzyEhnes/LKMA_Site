7ba2b2c1fea8a28248905352ee138cab3166e7f6
giobarrientos <giovannibarrien@csus.edu>
2022-10-20 22:51:39 -0700

    implemented hashing algorithm to database js

--- src/hash.js
+++ src/hash.js
@@ -1,7 +1,9 @@
-const bcrypt = require("bcryptjs");
+const bcrypt = require('bcryptjs');
 
-bcrypt.genSalt().then(salt => {
+bcrypt.genSalt(12).then(salt => {
     bcrypt.hash("password", salt).then(hash => {
+        console.log(salt);
         console.log(hash);
+        bcrypt.compare("password", hash).then(result => console.log(result));
     });
 })
