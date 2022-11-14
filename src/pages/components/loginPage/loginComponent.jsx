f5236f0f56c2cd05316d2aeee162b79dbdc639c2
izzy <izzyehnes@gmail.com>
2022-11-13 16:41:43 -0800

    LKMAW-183

--- src/pages/components/loginPage/loginComponent.jsx
+++ src/pages/components/loginPage/loginComponent.jsx
@@ -19,6 +19,7 @@ var validLogin = false;
 var login = false;
 var adminLogin = "";
 var createUser;
+var login_attempts = 5; 
 
 const validateEmail = (email) => {
   return String(email)
@@ -139,27 +140,15 @@ export const LoginComponent = (props) => {
     } else if (!(validateEmail(inputEmail.current.value))){
       console.log("Email invalid")
       document.getElementById("emailError").innerHTML = "Please enter a valid email"
-    }else{
+    } else{
       console.log("Email valid")
       document.getElementById("emailError").innerHTML = ""
 
       if(inputPassword.current.value === ""){
         console.log("No password provided")
         document.getElementById("passwordError").innerHTML = "Please provide a password"
-      } else if (inputPassword.current.value.length < 8){
-        console.log("Password must be 8 characters or longer in length")
-        document.getElementById("passwordError").innerHTML = "Password must be 8 characters or greater in length"
-      } else if (!(checkUppercase(inputPassword.current.value))){
-        console.log("Password must contain at least one uppercase letter")
-        document.getElementById("passwordError").innerHTML = "Password must contain at least one uppercase letter"
-      } else if (!(checkLowercase(inputPassword.current.value))){
-        console.log("Password must contain at least one lowercase letter")
-        document.getElementById("passwordError").innerHTML = "Password must contain at least one lowercase letter"
-      } else if (inputPassword.current.value.length > 7 && 
-        checkUppercase(inputPassword.current.value) && 
-        checkLowercase(inputPassword.current.value)) {
-          console.log("Valid Password")
-          document.getElementById("passwordError").innerHTML = ""
+      
+      } else {
           Login();
       }
     } 
@@ -203,18 +192,46 @@ export const LoginComponent = (props) => {
     
     if (validLogin) {
       login = true;
+
+      document.getElementById("passwordError").innerHTML = ""
+  
       logIn();
       adminLogIn();
 
       //populate adminLogin with the admin email address
       checkAdmin();
+      resetForm();
+
       return 0;      
     } else {
-      console.log("Incorrect login info. Please enter the correct email and password.")
-      document.getElementById("passwordError").innerHTML 
-        = "Incorrect login info. Please enter the correct email and password."
+
+      login_attempts --;
+
+			if(login_attempts == 0){
+				document.getElementById("email").disabled=true;
+				document.getElementById("password").disabled=true;
+				document.getElementById("signin").disabled=true;
+
+        document.getElementById("passwordError").innerHTML 
+        = "Too many incorrect log in attempts. Please try again later."
+			}
+
+      else {
+        console.log("Incorrect login info. Please enter the correct email and password.")
+        document.getElementById("passwordError").innerHTML 
+          = "Incorrect login info. Please enter the correct email and password."
+      }
     }
   }
+ 
+
+function resetForm(){
+	login_attempts = 5;
+	document.getElementById("email").disabled=false;
+	document.getElementById("password").disabled=false;
+	document.getElementById("signin").disabled=false;
+}
+ 
 
     return (
       <div id='login' className='text-center'>
@@ -246,7 +263,7 @@ export const LoginComponent = (props) => {
               </div>
               */}
               <div className="form-container sign-in-container">
-                <form action="#">
+                <form>
                   <h1>Sign in</h1>
                   <div id="emailError"></div>
                   <div id="passwordError"></div>
@@ -263,7 +280,7 @@ export const LoginComponent = (props) => {
                       Validate();
                   }} required/>
                   <a href="/forgot">Forgot your password?</a>
-                  <button type="button" data-testid="loginSubmit" 
+                  <button type="button" id="signin" data-testid="loginSubmit" 
                     onClick={MoveToProfile}>Sign In
                   </button>
                 </form>
